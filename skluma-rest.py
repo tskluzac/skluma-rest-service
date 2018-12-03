c2d7ecab4722f04a4f8852390059156b706d5a1f
tskluzac <skluzacek@uchicago.edu>
2018-12-03 16:11:18 +0100

    Job submission rewrite in here.

---
+++ skluma-rest.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Skluma is working! You've sent an empty request."
+
+
+@app.route('/<file_uuid>', methods=['GET'])
+def get_status(file_uuid):
+    return "Status for file {}:".format(file_uuid)
+
+
+# TODO (TYLER) for Tuesday.
+@app.route('/<globus_endpoint>/<file_path>', methods=['POST'])
+def submit_file(globus_endpoint, file_path):
+    # TODO: 1. Check whether endpoint exists.
+    # TODO: 2. Check whether filename exists.
+    # TODO: 2. Grab file from Globus and open.
+    # TODO: 3. Submit opened file for metadata extraction.
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
