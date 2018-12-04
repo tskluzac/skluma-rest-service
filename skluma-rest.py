52f28d3de5a983a2f9fc3a29aaadef70b88cb3e3
tskluzac <skluzacek@uchicago.edu>
2018-12-04 16:40:00 -0600

    Added response. TODO: Globus shared credentials.

--- skluma-rest.py
+++ skluma-rest.py
@@ -1,5 +1,13 @@
 
-from flask import Flask, request
+import globus_sdk
+import json
+import uuid
+
+
+from flask import Flask, request, jsonify, Response
+from mdf_forge.forge import Forge
+
+
 app = Flask(__name__)
 
 
@@ -16,12 +24,23 @@ def get_status(file_uuid):
 # TODO (TYLER) for Tuesday.
 @app.route('/<globus_endpoint>/<file_path>', methods=['POST'])
 def submit_file(globus_endpoint, file_path):
+
+    qualified_dns = "http://149.165.156.146"
+
+    file_uuid = uuid.uuid5(uuid.NAMESPACE_DNS, qualified_dns)
+    print(file_path + " " + str(file_uuid))
+
+    # TODO: Note, trying to 'transitively' use Globus auth via forge object from notebook.
+
     # TODO: 1. Check whether endpoint exists.
+
+
     # TODO: 2. Check whether filename exists.
     # TODO: 2. Grab file from Globus and open.
     # TODO: 3. Submit opened file for metadata extraction.
-    print("wow.")
 
+    # Return response that the job is accepted and the job is started.
+    return Response(json.dumps({'filepath_uuid': file_uuid}, status=202, mimetype='application/json'))
 
 
 
@@ -30,4 +49,4 @@ def submit_file(globus_endpoint, file_path):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
