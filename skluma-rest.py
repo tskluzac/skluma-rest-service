61a2fe59128bef5265fe4b5c16ec86af432be314
tskluzac <skluzacek@uchicago.edu>
2018-12-12 22:38:49 +0100

    Minor changes.

--- skluma-rest.py
+++ skluma-rest.py
@@ -3,7 +3,7 @@ import globus_sdk
 import json
 import uuid
 import sqs_queues
-
+import sys
 
 from flask import Flask, request, jsonify, Response
 from mdf_forge.forge import Forge
@@ -11,7 +11,11 @@ from mdf_forge.forge import Forge
 
 app = Flask(__name__)
 
-hardpath = "/home/tskluzac/Downloads/"
+# TODO: Move to skluma_cfg file.
+hard_path = "/home/tskluzac/Downloads/"
+
+# How we connect this REST API to Skluma codebase.
+sys.path.append('../skluma-local-deploy')
 
 
 @app.route('/', methods=['GET'])
@@ -29,16 +33,16 @@ def get_status(file_uuid):
 @app.route('/process_file/<filename>', methods=['POST'])
 def submit_file(filename):
 
+    # TODO: Move to skluma_cfg file.
     qualified_dns = "http://149.165.156.146"
 
     file_uuid = uuid.uuid5(uuid.NAMESPACE_DNS, qualified_dns)
 
     # TODO: Note, trying to 'transitively' use Globus auth via forge object from notebook.
 
-    # TODO: Deal with duplicate names.
-    full_path = hardpath + filename
+    full_path = hard_path + filename      # TODO: Deal with duplicate names (notebook side)
+
     submit_response = sqs_queues.sqs_producer(full_path, str(file_uuid))
-    print(submit_response)
 
     # TODO: 3. Save metadata to db. (in Skluma-local-deploy?)
 
