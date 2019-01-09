bb1828389edaf7cb8da8a7c5e3fa47486c6d19b7
tskluzac <skluzacek@uchicago.edu>
2019-01-09 16:26:44 -0600

    DB moved to object model.

--- db_utils.py
+++ db_utils.py
@@ -6,21 +6,21 @@ import datetime
 import sys
 
 sys.path.append('db_files')
-DATABASE = 'sklumadb4.db'
+DATABASE = 'db_files/sklumadb4.db'
 
 
 class SklumaDB:
-    def __init__(self, db_name):
+    def __init__(self):
         self.connected_bool = False
         self.conn = None
 
-        self.db_name = db_name
+        self.db_name = DATABASE
 
-    def connect_to_db(self, db_name):
+    def connect_to_db(self):
 
         if not self.connected_bool:
             try:
-                self.conn = sqlite3.connect(db_name)
+                self.conn = sqlite3.connect(self.db_name)
                 self.connected_bool = True
                 return self.conn
 
@@ -28,28 +28,14 @@ class SklumaDB:
                 print(e)
                 return self.conn
 
-    def insert_file(self, query_string):
+    def insert_file(self, conn, query_string):
         if self.conn is not None:
             try:
-                insert_into(self.conn, query_string)
+                insert_into(conn, query_string)
 
-            except:
-                print(" [DEBUG] PUT DB ERROR HERE. ")
+            except Error as e:
+                print(e)
 
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return None
 
 
 def select_all_files(conn):
@@ -69,14 +55,17 @@ def select_all_files(conn):
 
 def insert_into(conn, query_string):
 
+    print(query_string)
+
     try:
         cur = conn.cursor()
         cur.execute(query_string)
         conn.commit()
         return True
 
-    except:  # TODO: Find specific error-type.
+    except Error as e:  # TODO: Find specific error-type.
         print("[ERROR] Failure with DB_UTILS.INSERT_INTO command...")
+        print(e)
         return False
 
 
--- skluma-rest.py
+++ skluma-rest.py
@@ -1,12 +1,9 @@
 
-import datetime
 import db_utils
 import json
-import sqlite3
 import sys
 
 from flask import Flask, request, Response, g
-from sqlite3 import Error
 
 
 app = Flask(__name__)
@@ -15,10 +12,9 @@ app = Flask(__name__)
 hard_path = "/home/tskluzac/Downloads/"
 
 # How we connect this REST API to Skluma codebase.
-sys.path.append('../skluma-local-deploy')
 sys.path.append('db_files')
 
-DATABASE = 'sklumadb4.db'
+DATABASE = 'db_files/sklumadb4.db'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,25 +31,22 @@ def get_job_status(job_uuid):
 @app.route('/process_file', methods=['POST'])
 def submit_file():
 
-    skluma_db = db_utils.SklumaDB(DATABASE)
-
     file_data = json.loads(request.data)
+    # Leave here to have an easily-accessible offline source of data.
+    # file_data = {"task_id": "a", "job_id": "b", "file_path": "c", "uniq_path": "d"}
 
     task_id = file_data["task_id"]
     job_id = file_data["job_id"]
     file_path = file_data["file_path"]
     uniq_path = file_data["uniq_path"]
 
-    try:
+    # Get instance of SklumaDB class, connect to DB, and insert metadata.
+    skluma_db = db_utils.SklumaDB()
+    skluma_conn = skluma_db.connect_to_db()
+    insert_success = skluma_db.insert_file(skluma_conn, db_utils.make_insert_string(task_id, job_id, file_path, uniq_path))
 
-        # Create file entry in database.
-        init_query = "INSERT INTO sklumadb4 (task_id, job_id, cur_status, subm_time, real_path, req_path) " \
-                     "VALUES ({0}, {1}, {2}, {3}, {4}, {5});".format(str(task_id), str(job_id), str('TRANSFER'), str(datetime.datetime.now()), file_path, uniq_path)
-
-
-
-    except:
-        return Response('BAD')
+    if not insert_success:
+        raise LookupError("[ERROR] Unable to add file.")
 
     return Response(json.dumps({"task_id": task_id, "job_id": job_id, "process": "SUBMITTED"}))
 
@@ -63,6 +56,7 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
